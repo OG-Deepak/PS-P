2706c285ff0618002bfa04795ca04015994fbeaf
OG-Deepak <deepakdevan458@gmail.com>
2026-08-24 01:28:45 +0530

    Add common.c to find common elements in arrays

---
+++ lab 2/common.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[] = {10, 20, 30, 40, 50};
+    int arr2[] = {30, 40, 60, 70, 50};
+
+    int n1 = 5;
+    int n2 = 5;
+
+    printf("Common elements: ");
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+
+            if (arr1[i] == arr2[j]) {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
